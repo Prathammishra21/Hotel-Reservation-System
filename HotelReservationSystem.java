ca1ac7edd497516cd3c9a927fbf0caa544400744
Pratham Mishra <mishrapratham119@gmail.com>
2024-03-25 23:49:09 +0530

    Added mysql jar file and configured jdbc

--- HotelReservationSystem.java
+++ HotelReservationSystem.java
@@ -1,5 +1,16 @@
+import java.sql.SQLException;
+
 public class HotelReservationSystem {
-    public static void main(String[] args) {
-        System.out.println("");
+
+    private static final String url="jdbc:mysql://localhost:3306/hotel_db";
+    private static final String username = "root";
+    private static final String password = "Pratham_1308";
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch (ClassNotFoundException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
