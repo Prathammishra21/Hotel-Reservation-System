bd383c54e649ce346924a845be8d7e6bfb626afa
Pratham Mishra <mishrapratham119@gmail.com>
2024-03-29 10:22:46 +0530

    Updated

--- HotelReservationSystem.java
+++ HotelReservationSystem.java
@@ -1,84 +1,65 @@
-import jdk.jshell.SourceCodeAnalysis;
-
 import java.sql.*;
 import java.util.Scanner;
 
-import static java.lang.System.exit;
-
 public class HotelReservationSystem {
 
-    private static final String url="jdbc:mysql://localhost:3306/hotel_db";
-    private static final String username = "root";
-    private static final String password = "Pratham_1308";
+    private static final String URL = "jdbc:mysql://localhost:3306/hotel_db";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Pratham_1308";
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        try{
+    public static void main(String[] args) {
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-        }catch (ClassNotFoundException e){
-            System.out.println(e.getMessage());
-        }
-
-        try{
-            Connection connection = DriverManager.getConnection(url, username, password);
-            Scanner scanner = new Scanner(System.in);
-            while(true){
-                System.out.println();
-                System.out.println("HOTEL MANAGEMENT SYSTEM");
-                Scanner sc = new Scanner(System.in);
-                System.out.println("1. Reserve your room");
-                System.out.println("2. View Reservations");
-                System.out.println("3. Get Room Number");
-                System.out.println("4. Update Reservations");
-                System.out.println("5. Delete Reservations");
-                System.out.println("0. Exit");
-                System.out.println("Choose an option: ");
-                int choice = scanner.nextInt();
-                switch(choice){
-                    case 1:
-                        reserveRoom(connection, scanner);
-                        break;
-                    case 2:
-                        viewReservations(connection);
-                        break;
-                    case 3:
-                        getRoomNumber(connection, scanner);
-                        break;
-                    case 4:
-                        updateReservation(connection, scanner);
-                        break;
-                    case 5:
-                        deleteReservation(connection, scanner);
-                        break;
-                    case 0:
-                        exit();
-                        scanner.close();
-                        return;
-                    default:
-                        System.out.println("Invalid choice. Try again.");
-                }
+            try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                 Scanner scanner = new Scanner(System.in)) {
+                handleUserInput(connection, scanner);
             }
-        }catch (SQLException e){
-            System.out.println(e.getMessage());
-        }catch(InterruptedException e){
-            throw new RuntimeException(e);
+        } catch (ClassNotFoundException | SQLException | InterruptedException e) {
+            System.out.println("Error: " + e.getMessage());
         }
     }
 
-    private static void reserveRoom(Connection connection, Scanner scanner){
-        try{
+    private static void handleUserInput(Connection connection, Scanner scanner) throws SQLException, InterruptedException {
+        while (true) {
+            System.out.println("\nHOTEL MANAGEMENT SYSTEM");
+            System.out.println("1. Reserve your room");
+            System.out.println("2. View Reservations");
+            System.out.println("3. Get Room Number");
+            System.out.println("4. Update Reservations");
+            System.out.println("5. Delete Reservations");
+            System.out.println("0. Exit");
+            System.out.println("Choose an option: ");
+
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1 -> reserveRoom(connection, scanner);
+                case 2 -> viewReservations(connection);
+                case 3 -> getRoomNumber(connection, scanner);
+                case 4 -> updateReservation(connection, scanner);
+                case 5 -> deleteReservation(connection, scanner);
+                case 0 -> exit();
+                default -> System.out.println("Invalid choice. Try again.");
+            }
+        }
+    }
+
+    private static void reserveRoom(Connection connection, Scanner scanner) {
+        try {
             System.out.println("Enter guest name: ");
             String guestName = scanner.next();
-            scanner.nextLine();
             System.out.println("Enter room number: ");
             int roomNumber = scanner.nextInt();
             System.out.println("Enter contact number: ");
             String contactNumber = scanner.next();
 
-            String sql = "Insert Into reservations (guest_name, room_number, contact_number)" +
-                    "Values ('"+guestName+"',"+roomNumber+",'"+contactNumber+"')";
+            String sql = "INSERT INTO reservations (guest_name, room_number, contact_number) VALUES (?, ?, ?)";
 
-            try (Statement statement = connection.createStatement()) {
-                int affectedRows = statement.executeUpdate(sql);
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setString(1, guestName);
+                statement.setInt(2, roomNumber);
+                statement.setString(3, contactNumber);
+
+                int affectedRows = statement.executeUpdate();
 
                 if (affectedRows > 0) {
                     System.out.println("Reservation successful!");
@@ -87,7 +68,7 @@ public class HotelReservationSystem {
                 }
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println("Error: " + e.getMessage());
         }
     }
 
@@ -95,7 +76,7 @@ public class HotelReservationSystem {
         String sql = "SELECT reservation_id, guest_name, room_number, contact_number, reservation_date FROM reservations";
 
         try(Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(sql)){
+            ResultSet resultSet = statement.executeQuery(sql)){
 
             System.out.println("Current Reservations:");
             System.out.println("+----------------+---------------+---------------+----------------------+-------------------------+");
@@ -181,21 +162,6 @@ public class HotelReservationSystem {
         }
     }
 
-    private static boolean reservationExists(Connection connection, int reservationId) {
-        try {
-            String sql = "SELECT reservation_id FROM reservations WHERE reservation_id = " + reservationId;
-
-            try (Statement statement = connection.createStatement();
-                 ResultSet resultSet = statement.executeQuery(sql)) {
-
-                return resultSet.next();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-
     private static void deleteReservation(Connection connection, Scanner scanner) {
         try {
             System.out.print("Enter reservation ID to delete: ");
@@ -233,5 +199,26 @@ public class HotelReservationSystem {
         System.out.println();
         System.out.println("ThankYou For Using Hotel Reservation System!!!");
     }
+
+    private static boolean reservationExists(Connection connection, int reservationId) {
+        try {
+            String sql = "SELECT reservation_id FROM reservations WHERE reservation_id = " + reservationId;
+
+            try (Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery(sql)) {
+
+                return resultSet.next();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
+
+
+
+
+
+
 
